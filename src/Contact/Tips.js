095666cc06b82c1a89fedea2d979831fbf57034e
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-02-18 23:44:39 +0530

    contact copy ,img modal added

--- src/Contact/Tips.js
+++ src/Contact/Tips.js
@@ -2,16 +2,27 @@
 const Tips = [
     {
         title: '1. Always be aware of the unsafe neighborhood',
+        no: `img2`,
+
+        img: "https://allswellalert.com/uploads/blog/dd82a10ef0847039e8eef524f0a88f1c.png",
         desc: `At times, we have to take the unknown route or travel to unknown places. This can be dangerous! Knowing the danger zones in advance helps to avoid those lines or to walk with precautions. Rapists are sexual predators.They are nothing like humans. It is important to make sensible decisions and avoid unwanted situations.`
     },
     {
         title: '2. Know the route if you are travelling to an unsafe place.',
+        no: "img2",
+
+        img: 'https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2020/02/ia_0924_recommended-routes-for-solo-female-travellers-according-to-women-at-culture-trip_header_gs.jpg',
         desc: `Travelling to a new place might involve taking a lonely road.With all the technologies at your disposal, it is smart to check the route.Read a few reviews on the net.If theroute seems eerie, then make arrangements of your safety.After all, your safety is your first responsibility.Always note the number of the vehicle you are boarding.Share the details with someone over the phone.So,even the driver knows that his details are being traced.`
     }, {
         title: '3. Learn some kind of self-defense tactics.',
+        no: "img3",
+
+        img: 'https://i.pinimg.com/736x/c5/87/f1/c587f193f70aaf50414f5e10504cdd3f.jpg',
         desc: `Learning self- defense is no more an extra curriculum skill.It is a survival skill in this modern day.Learning a few basic self defense tricks can prove to be life - saving.Be it karate, martial arts or Kalaripayattu! Enroll your name along with your kid's.It can save both your lives! Also, there are multiple sessions happening on this around the city.Attend those and learn tricks to be safe!`
     }, {
         title: '4. Always carry some kind of sharp objects at your hands reach.',
+        no: "img4",
+        img: 'https://st3.depositphotos.com/2704315/13872/v/600/depositphotos_138728932-stock-illustration-woman-character-protected-herself-from.jpg',
         desc: `
     Be it a foldable knife or a nail cutter! A sharp cut distracts the predator giving you time to escape the scene.
    Always carry a sharp object in your purse when travelling.If you have long nails then learn to use them for self -
@@ -23,6 +34,8 @@ const Tips = [
    `
     }, {
         title: '5. Danger in a life? Press all buttons!',
+        no: "img5",
+        img: 'https://www.safetyandhealthmagazine.com/ext/resources/images/2018/02-feb/woman-by-elevator.jpg',
         desc: `
     Most of the time, women get the instinct when she is in danger.If any such situation arises in the lift then press
     all the buttons until the destination.
@@ -31,6 +44,8 @@ const Tips = [
    `
     }, {
         title: '6. When you are alone in the house, run into the kitchen.',
+        no: "img6",
+        img: 'https://roofandfloor.thehindu.com/raf/real-estate-blog/wp-content/uploads/sites/14/2019/07/Five-Safety-Tips-for-Women-Living-Alone-in-a-Rented-House-840x480.jpg',
         desc: `
     You know where the chilli powder and turmeric are kept.And where the knives and plates are.
     All these can be turned into deadly weapons.If nothing else, start throwing plates and utensils all over.Let
@@ -39,6 +54,8 @@ const Tips = [
     `
     }, {
         title: '7. Avoid getting drunk and unconscious at unsafe places.',
+        no: "img7",
+        img: `https://images.unsplash.com/photo-1519671482749-fd09be7ccebf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`,
         desc: `
     With night parties picking the rage, don't forget to think about your safety.
      Don't get drunk and risk your life with strangers. Always enjoy within your limits.
@@ -47,6 +64,8 @@ yourself, "My safety is my responsibility.
     `
     }, {
         title: '8. Avoid empty bus and locals at late night',
+        no: 'img8',
+        img: 'https://img.freepik.com/premium-vector/bus-stop-flat-vector-illustration-people-waiting-bus-man-riding-bicycle-cartoon-characters-urban-transportation-means-public-transport-skyscrapers-background-city-infrastructure_126283-2651.jpg',
         desc: `
     After the jittering Nirbhaya case, no girl dares to take an empty bus or board an empty local.
     This is going to remain the same! Until the country comes up with something so strong that the predators fear
@@ -54,12 +73,16 @@ yourself, "My safety is my responsibility.
     `
     }, {
         title: '9. If you are stalked at night rush to a shop or an ATM.',
+        no: "img9",
+        img: 'https://images.unsplash.com/photo-1483884105135-c06ea81a7a80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybCUyMGFsb25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
         desc: `
     Enter a shop or a house or go inside an ATM box.ATM centres always have close circuit television.Fearing
 identification, no one will dare attack you.
    `
     }, {
         title: '10. If the driver takes a wrong turn, use your dupatta or the handle of your purse to choke him.',
+        no: "img10",
+        img: 'https://i0.wp.com/picjumbo.com/wp-content/uploads/woman-driving-at-night.jpg',
         desc: `
     Use the handle of your purse or your stole to wrap around his neck.And then pull him back.
     This will make him choke instantly.
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,16 @@ module.exports = {
     extend: {
       backgroundImage: {
         "img1": 'url("https://allswellalert.com/uploads/blog/dd82a10ef0847039e8eef524f0a88f1c.png")',
-        "img2": 'url("https://vid.alarabiya.net/images/2017/09/19/a7f51684-691d-4e1c-8779-1f54b3a61ae9/a7f51684-691d-4e1c-8779-1f54b3a61ae9_16x9_1200x676.jpg?width=801&format=jpg")',
-        "img3": 'url()'
+        "img2": 'url("https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2020/02/ia_0924_recommended-routes-for-solo-female-travellers-according-to-women-at-culture-trip_header_gs.jpg")',
+        "img3": 'url("https://i.pinimg.com/736x/c5/87/f1/c587f193f70aaf50414f5e10504cdd3f.jpg")',
+        "img4": 'url("https://st3.depositphotos.com/2704315/13872/v/600/depositphotos_138728932-stock-illustration-woman-character-protected-herself-from.jpg")',
+        "img5": 'url("https://www.safetyandhealthmagazine.com/ext/resources/images/2018/02-feb/woman-by-elevator.jpg")',
+        "img6": 'url("https://roofandfloor.thehindu.com/raf/real-estate-blog/wp-content/uploads/sites/14/2019/07/Five-Safety-Tips-for-Women-Living-Alone-in-a-Rented-House-840x480.jpg")',
+        "img7": 'url("https://us.123rf.com/450wm/artinspiring/artinspiring1802/artinspiring180200243/95341604-isolated-drunk-woman-having-fun-with-wine-bottle.jpg")',
+        "img8": 'url("https://img.freepik.com/premium-vector/bus-stop-flat-vector-illustration-people-waiting-bus-man-riding-bicycle-cartoon-characters-urban-transportation-means-public-transport-skyscrapers-background-city-infrastructure_126283-2651.jpg")',
+        "img9": 'url("https://media.istockphoto.com/id/1341651858/vector/women-enjoy-shopping-in-mall-banner-or-background.jpg?s=612x612&w=0&k=20&c=jURCGlU9c3Xw4RU-RntoEiBlphcqMtf8w-D84w9BDBo=")',
+        "img10": 'url("https://i0.wp.com/picjumbo.com/wp-content/uploads/woman-driving-at-night.jpg")',
+
       },
       colors: {
         bcc: {
